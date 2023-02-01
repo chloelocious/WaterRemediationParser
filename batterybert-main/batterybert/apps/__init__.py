8e70267eed8e59642dbc674247fb3589bd12da60
chloecwn <93173038+chloelocious@users.noreply.github.com>
2023-02-01 09:38:53 -0500

    no bert

--- batterybert-main/batterybert/apps/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-batterybert.apps.__init__
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-author: Shu Huang (sh2009@cam.ac.uk)
-"""
-from .classify import DocClassifier
-from .qa import DeviceDataExtractor, QAAgent
